04ff7f61d3d6922e9c359aba3662d2c35e9e81c3
sylvi <20102981@mail.wit.ie>
2025-10-11 11:13:12 +0100

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -3,7 +3,7 @@ import { getUpcomingMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import AddToWatchIcon from '../components/cardIcons/addToWatch'
 
 const UpcomingMoviesPage = () => {
    const { data, error, isPending, isError  } = useQuery({
@@ -26,7 +26,7 @@ const UpcomingMoviesPage = () => {
         title="Upcoming Movies"
         movies={movies}
         action={(movie) => {
-          return <AddToFavoritesIcon movie={movie} />
+          return <AddToWatchIcon movie={movie} />
         }}
       />
   );
